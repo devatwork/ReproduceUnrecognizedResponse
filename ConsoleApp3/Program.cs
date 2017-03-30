969181566d9ad0592fd880399a637d52eacd5f4d
Jos Verburg <jos.verburg@liones.nl>
2017-03-30 09:29:22 +0200

    Initial commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+
+namespace ConsoleApp3
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			for (var i = 0; i < 10000; i++)
+			{
+				using (var httpClient = new HttpClient())
+				{
+					var request = new HttpRequestMessage(HttpMethod.Head, new Uri("http://test.com:5000"));
+					var response = httpClient.SendAsync(request).Result;
+				}
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Configure both the ConsoleApp3 and WebApplication1 as startup projects and run to reproduce the "The server returned an invalid or unrecognized response" error.
