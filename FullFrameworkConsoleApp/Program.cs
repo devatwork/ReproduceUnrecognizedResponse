e522fb4fa1acba65db4b64f0435f551eb699ea33
devatwork <b.willems@premotion.nl>
2017-04-03 11:30:55 +0200

    Add full framework console app

---
+++ FullFrameworkConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+
+namespace FullFrameworkConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			for (var i = 0; i < 10000; i++)
+			{
+				using (var httpClient = new HttpClient())
+				{
+					var request = new HttpRequestMessage(HttpMethod.Head, new Uri("http://127.0.0.1:5000"));
+					var response = httpClient.SendAsync(request).Result;
+				}
+			}
+		}
+    }
+}
